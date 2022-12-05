fea7fdae36fca84c88d7d1b0d63ee3139ead080b
Nicholas Burrell <nicholas.burrell@protonmail.com>
2022-12-05 03:20:55 -0800

    Remove CORS check

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,7 @@ const port = process.env.SERVER_PORT;
 app.use(express.json());
 
 app.use(function(req,res,next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
